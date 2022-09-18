4d81675ddfc7c35370529414de35517bb449df7d
unknown <Duonggiaphu09@gmail.com>
2022-09-18 14:58:24 +0700

    first commit

---
+++ Happy-birthday-main/config.js
@@ -0,0 +1,7 @@
+const nameGirl = 'Vợ Iu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Bé';
+const contentCard = 'Chúc bé của anh một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Anh yêu em <3';
+const giftImage = '2.png';
+const giftImageBase64 = "data:image/png;base64, " + base64;
